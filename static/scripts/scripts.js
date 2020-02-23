2f9bfc581765d11aebb8e80850f803e6d0fb8ece
Sarani1612 <sara.larsen1612@gmail.com>
2020-02-23 10:10:15 +0000

    added function to remove alerts after 5 seconds

--- static/scripts/scripts.js
+++ static/scripts/scripts.js
@@ -1,20 +1,27 @@
-$(document).ready(function() {
-// Opens modal when user clicks on the button to edit a recipe
-var modal = document.getElementById("edit-modal");
-var button = document.getElementById("modal-button");
+$(document).ready(function () {
 
-button.onclick = function() {
-  modal.style.display = "block";
-}
+    // Opens modal when user clicks on the button to edit a recipe
+    var modal = document.getElementById("edit-modal");
+    var button = document.getElementById("modal-button");
 
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+    button.onclick = function () {
+        modal.style.display = "block";
+    };
 
+    // Closes modal when clicking outside of it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    };
 
-  if ( $('#wrong_key').length ) { // if there is an DOM that has class has-error
-     modal.style.display = "block"; // Show Modal
-  }
-});
+    // Ensures modal is kept open if the wrong key was entered to avoid having to click the button
+    if ($('#wrong_key').length)
+        modal.style.display = "block";
+
+    // Removes flash messages after 5 seconds
+    setTimeout(function () {
+        $('.alert').remove()}, 5000
+        )
+
+    });
