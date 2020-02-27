e8220dc066426846c6f66108d55f22fedd816e8d
Sarani1612 <sara.larsen1612@gmail.com>
2020-02-27 12:31:58 +0000

    added missing semi-colon

--- static/scripts/scripts.js
+++ static/scripts/scripts.js
@@ -2,9 +2,9 @@ window.onload = function(){
 
     // Opens modal when user clicks on the button to edit a recipe
     var modal = document.getElementById("edit-modal");
-    var button = document.getElementById("modal-button");
+    var modalBtn = document.getElementById("modal-button");
 
-    button.onclick = function () {
+    modalBtn.onclick = function () {
         modal.style.display = "block";
     };
 
@@ -22,6 +22,6 @@ window.onload = function(){
     // Removes flash messages after 5 seconds
     setTimeout(function () {
         $('.alert').remove();}, 5000
-        )
-
-    };
+        );
+        
+};
